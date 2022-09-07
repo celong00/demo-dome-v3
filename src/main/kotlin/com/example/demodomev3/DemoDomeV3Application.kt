38ab67aa6b2cccd16e42894dfcac19f93bec95ec
celong00 <michaelalexanderrustan@gmail.com>
2022-09-07 16:36:52 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/demodomev3/DemoDomeV3Application.kt
@@ -0,0 +1,11 @@
+package com.example.demodomev3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoDomeV3Application
+
+fun main(args: Array<String>) {
+	runApplication<DemoDomeV3Application>(*args)
+}
---
+++ src/test/kotlin/com/example/demodomev3/DemoDomeV3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demodomev3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoDomeV3ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
